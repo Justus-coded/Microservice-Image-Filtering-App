1129a9d0f8d753526669eb3c2b7dfdbe16aec879
Justus-coded <justusilemobayo@gmail.com>
2022-08-30 23:40:10 +0100

    Added project files

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mydb
+export POSTGRES_PASSWORD=justus2022
+export POSTGRES_HOST=mydb.cawwnlw8sppx.us-east-1.rds.amazonaws.com
+export AWS_BUCKET=justdev1
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
